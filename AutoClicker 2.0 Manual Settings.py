e7262b66a2c37f27e972d42460b9baa3bde25ad1
SkeletronTT <62153446+SkeletronTT@users.noreply.github.com>
2022-01-19 13:42:15 +0500

    NonRelise Ver

--- AutoClicker 2.0 Manual Settings.py
+++ AutoClicker 2.0 Manual Settings.py
@@ -3,27 +3,39 @@ import pyautogui as pg
 import time
 from mss import mss
 
+# Вывод настроек
+with open('settings.txt') as fs:
+    lines = fs.readlines()
+Left = lines[6]
+Top = lines[7]
+Width = lines[11]
+Height = lines[12]
+R = lines[17]
+G = lines[18]
+B = lines[19]
+
 # Захват экрана
 monitor = {
-        "left": 1154,
-        "top": 285,
-        "width": 361,
-        "height": 478,
+        "left": Left,
+        "top": Top,
+        "width": Width,
+        "height": Height,
 }
+print(monitor)
 def FindColor(fcol, monitor={}):
     #85 172 238
     m = mss()
     img = m.grab(monitor)
     img_arr = np.array(img)
 
-    #Поиск цвета (b, g, r, a)
+    #Поиск цвета (blue, green, red, alpha)
     fmap = (fcol[2], fcol[1], fcol[0], 255)
     indx = np.where(np.all(img_arr == fmap, axis=-1))
     fcrd = np.transpose(indx)
     return fcrd
 
-#Искомый цвет
-fcol = (224, 67, 167)
+# Искомый цвет
+fcol = (R, G, B)
 
 while True:
     time1 = time.time()
@@ -34,6 +46,4 @@ while True:
         y = result[0][0]+monitor.get('top')
         print(time2 - time1, [x,y])
         pg.moveTo(x,y)
-        pg.moveRel(3, 3)
-        pg.click()
-    time.sleep(0.1)
+    time.sleep(3)
